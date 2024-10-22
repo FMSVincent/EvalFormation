c8a3f8d8902ff7e982698472d86c9df88314bd41
vincent <vincent.bickel@fms-ea.com>
2024-10-22 17:58:09 +0200

    create file config / and connection to bdd formation

--- src/fr/fms/FormationApp.java
+++ src/fr/fms/FormationApp.java
@@ -3,7 +3,6 @@ package fr.fms;
 public class FormationApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module EvalFormation {
+	requires java.sql;
 }
